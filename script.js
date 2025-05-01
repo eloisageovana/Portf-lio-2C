1ce2d4deb163f43abe2067b38d366065a17d26d7
eloisageovana <prod.egsilva@gmail.com>
2025-05-01 14:21:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+window.onscroll = function() {
+    const topBtn = document.getElementById("topBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+};
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
